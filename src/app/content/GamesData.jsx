17ffdfc6498655b536511459175b904a1887541f
pawan <pawanpatel12895@gmail.com>
2023-07-03 19:42:28 +0530

    Added Sample dummy Data

---
+++ src/app/content/GamesData.jsx
@@ -0,0 +1,14 @@
+// GamesData.jsx
+
+const GamesData = () => {
+  const content = [
+    { data: 'Game 1', url: '/games/game1' },
+    { data: 'Game 2', url: '/games/game2' },
+    { data: 'Game 3', url: '/games/game3' },
+    // Add more items as needed
+  ];
+
+  return content; 
+};
+
+export default GamesData;
